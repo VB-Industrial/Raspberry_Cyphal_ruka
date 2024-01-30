9459371dfcbf1f4bc0d2ba298758cb516568994a
Sorokonog <roman@voltbro.com>
2024-01-30 19:46:59 +0300

    up->down proto

---
+++ hardware/ruka_hardware.cpp
@@ -0,0 +1,186 @@
+
+
+#include "ruka_hardware.hpp"
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+#include <iomanip> 
+#include "cyphal/allocators/o1/o1_allocator.h"
+#include "cyphal/cyphal.h"
+#include "cyphal/providers/LinuxCAN.h"
+#include "cyphal/subscriptions/subscription.h"
+#include "uavcan/node/Heartbeat_1_0.h"
+#include "reg/udral/physics/kinematics/rotation/Planar_0_1.h"
+
+
+#define JS_PUB_PORT_ID 1337
+#define JS_SUB_PORT_ID 1338
+#define JOINT_N_1 1
+
+TYPE_ALIAS(HBeat, uavcan_node_Heartbeat_1_0)
+TYPE_ALIAS(JS_msg, reg_udral_physics_kinematics_rotation_Planar_0_1)
+
+std::byte buffer[sizeof(CyphalInterface) + sizeof(LinuxCAN) + sizeof(O1Allocator)];
+
+void error_handler() {std::cout << "error" << std::endl; std::exit(EXIT_FAILURE);}
+uint64_t micros_64() {
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+UtilityConfig utilities(micros_64, error_handler);
+
+std::shared_ptr<CyphalInterface> cy_interface;
+      // 1000 msgs combined pool
+
+
+uint32_t uptime = 0;
+void heartbeat() {
+    static uint8_t hbeat_buffer[HBeat::buffer_size];
+    static CanardTransferID hbeat_transfer_id = 0;
+    HBeat::Type heartbeat_msg = {.uptime = uptime, .health = {uavcan_node_Health_1_0_NOMINAL}, .mode = {uavcan_node_Mode_1_0_OPERATIONAL}};
+    cy_interface->send_msg<HBeat>(&heartbeat_msg, hbeat_buffer, uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, &hbeat_transfer_id);
+    uptime += 1;
+}
+
+namespace joint_trajectory_controller
+{
+CallbackReturn JointTrajectoryController::on_init(const hardware_interface::HardwareInfo & info)
+{
+  if (hardware_interface::SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
+  {
+    return CallbackReturn::ERROR;
+  }
+
+  cy_interface = CyphalInterface::create_heap<LinuxCAN, O1Allocator>(100, "can0", 1000, utilities);
+
+  // robot has 6 joints and 2 interfaces
+  joint_position_.assign(6, 0);
+  joint_velocities_.assign(6, 0);
+  joint_position_command_.assign(6, 0);
+  joint_velocities_command_.assign(6, 0);
+
+  // force sensor has 6 readings
+  ft_states_.assign(6, 0);
+  ft_command_.assign(6, 0);
+
+  for (const auto & joint : info_.joints)
+  {
+    for (const auto & interface : joint.state_interfaces)
+    {
+      joint_interfaces[interface.name].push_back(joint.name);
+    }
+  }
+
+  return CallbackReturn::SUCCESS;
+}
+
+
+
+std::vector<hardware_interface::StateInterface> JointTrajectoryController::export_state_interfaces()
+{
+  std::vector<hardware_interface::StateInterface> state_interfaces;
+
+  int ind = 0;
+  for (const auto & joint_name : joint_interfaces["position"])
+  {
+    state_interfaces.emplace_back(joint_name, "position", &joint_position_[ind++]);
+  }
+
+  ind = 0;
+  for (const auto & joint_name : joint_interfaces["velocity"])
+  {
+    state_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_[ind++]);
+  }
+
+  state_interfaces.emplace_back("tcp_fts_sensor", "force.x", &ft_states_[0]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "force.y", &ft_states_[1]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "force.z", &ft_states_[2]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "torque.x", &ft_states_[3]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "torque.y", &ft_states_[4]);
+  state_interfaces.emplace_back("tcp_fts_sensor", "torque.z", &ft_states_[5]);
+
+  return state_interfaces;
+}
+
+std::vector<hardware_interface::CommandInterface> JointTrajectoryController::export_command_interfaces()
+{
+  std::vector<hardware_interface::CommandInterface> command_interfaces;
+
+  int ind = 0;
+  for (const auto & joint_name : joint_interfaces["position"])
+  {
+    command_interfaces.emplace_back(joint_name, "position", &joint_position_command_[ind++]);
+  }
+
+  ind = 0;
+  for (const auto & joint_name : joint_interfaces["velocity"])
+  {
+    command_interfaces.emplace_back(joint_name, "velocity", &joint_velocities_command_[ind++]);
+  }
+
+  command_interfaces.emplace_back("tcp_fts_sensor", "force.x", &ft_command_[0]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "force.y", &ft_command_[1]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "force.z", &ft_command_[2]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "torque.x", &ft_command_[3]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "torque.y", &ft_command_[4]);
+  command_interfaces.emplace_back("tcp_fts_sensor", "torque.z", &ft_command_[5]);
+
+  return command_interfaces;
+}
+
+return_type JointTrajectoryController::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+{
+  // TODO(pac48) set sensor_states_ values from subscriber
+
+  for (auto i = 0ul; i < joint_velocities_command_.size(); i++)
+  {
+    joint_velocities_[i] = joint_velocities_command_[i];
+    joint_position_[i] += joint_velocities_command_[i] * period.seconds();
+  }
+
+  for (auto i = 0ul; i < joint_position_command_.size(); i++)
+  {
+    joint_position_[i] = joint_position_command_[i];
+  }
+
+  cy_interface->loop();
+  return return_type::OK;
+}
+
+int itera = 0;
+return_type JointTrajectoryController::write(const rclcpp::Time &, const rclcpp::Duration &)
+{
+
+ // for (auto i = 0ul; i < joint_velocities_command_.size(); i++) {
+    // Simulate sending commands to the hardware
+ //   printf("Got command %.5f for joint %li \n", joint_position_[i], i);
+
+  //}
+ if (itera > 100)
+ {
+    heartbeat();
+    itera = 0;
+  }
+  std::cout<<"pamagiti"<<std::endl;
+  cy_interface->loop();
+  itera++;
+  return return_type::OK;
+}
+
+}  // namespace ros2_control_demo_example_7
+
+#include "pluginlib/class_list_macros.hpp"
+
+
+PLUGINLIB_EXPORT_CLASS(
+  joint_trajectory_controller::JointTrajectoryController, hardware_interface::SystemInterface)
+
+
+
+
+
+
